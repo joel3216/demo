152b536589fb31dd985d21a3038cd65eec02d1b8
joel3216 <joeljaxon3216@gmail.com>
2020-12-23 16:19:45 +0530

    testing dictionary

---
+++ empWageDictionary.sh
@@ -0,0 +1,46 @@
+#! /bin/bash -x
+
+isFullTime=1
+isPartTime=2
+wagePerHr=20
+maxHrs=50
+maxDays=20
+declare -A dailyWage
+
+function getEmpHrs(){
+	local empCheck=$1
+	case $empCheck in
+		 $isFullTime)
+			empHrs=$(($empHrs +8))
+				 ;;
+		 $isPartTime)
+			empHrs=$(($empHrs +4))
+				 ;;
+	 	*)
+			empHrs=$(($empHrs +0))
+				 ;;
+	esac
+	echo $empHrs
+
+}
+
+function wageCalculation(){
+local empHrs=$1
+local wagePerHr=$2
+salary=$(( $empHrs*$wagePerHr ))
+echo $salary
+}
+
+while [[ $empHrs -le $maxHrs && $day -le $maxDays ]]
+do
+	empCheck=$((RANDOM%3))
+	day=$(( $day+1 ))
+	empHrs=$( getEmpHrs $empCheck )
+	salary=$(( $empHrs*$wagePerHr ))
+	dailyWage["day "$day]=$( wageCalculation $empHrs $wagePerHr )
+	totalSalary=$(( $totalSalary+$salary ))
+
+done
+
+echo ${dailyWage[@]}
+echo ${!dailyWage[@]}
